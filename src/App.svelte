209c1fe792f271a1549658ee9f7beae62023d8b9
Garret <me@unsmart.co>
2020-07-15 20:41:54 +0000

    init: brime is fake!

---
+++ src/App.svelte
@@ -0,0 +1,99 @@
+<style>
+	h1 {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	p, h2 {
+		margin: 1em auto;
+	}
+  
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+
+  main {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+</style>
+
+<svelte:head>
+	<title>BrimeLive</title>
+  <meta name="description" content="Let's talk about Brime Live...">
+
+  <meta property="og:title" content="BrimeLive" />
+  <meta property="og:description" content="Let's talk about Brime Live..." />
+</svelte:head>
+
+<main>
+  <h1>Brime Brime Brime...</h1>
+
+  <p>
+
+  For those who may not know, <a href="https://twitter.com/WatchBrime">Brime</a> is a supposed new streaming platform. 
+  They have claimed to be all that and what every streamer needs. However, just from following their social accounts it 
+  can be made very obvious this dream of a platform is an account in the hands of someone who has no idea what they are doing.
+
+  </p>
+
+  <h2>Site "Testing"</h2>
+
+  <p>
+
+  The most obvious part of this entire ordeal where it can be blatently obvious how fake it is at least to any developer
+  is the site "load testing" they did. The events took place on July 7th-8th starting around 8PM and ending around 1AM central time.
+  Load testing is definitely a very thing to do as you need to know that your site can handle a lot of load and not crash.
+  However, any developer would know you do not test this by using a shared hosted wordpress site that is only serving static
+  content to users. Static content being things that never change and can be cached on the users browser where a refresh will
+  not recall the server to get an update on the file. The worst part of all of this, however, was they were using a 3$ a month
+  shared hosting wordpress site from hostboogie. Shared hosting means that the Brime site was one of many sites running on the same
+  server. This allows hosting companies to offer extremely cheap pricing for sites which may not receive a lot of traffic, thus not 
+  needing a dedicated server to handle serving the content to users
+  (<a href="https://twitter.com/lucidic/status/1280667695063732225?s=21">Here is a link to tweet proving they were using shared hosting</a>).
+
+  </p>
+
+  <h2>No Information Given</h2>
+
+  <p>
+
+  To amplify all of this Brime has never really given any real information about their platform. They have not shown a single preview
+  of what the site will look like. Any real business would always at least give a little bit more into an insight of what they are 
+  actually doing and have something real to show rather than a "just take our word for it" attitude. Or at the very least they would
+  be understanding of the people who think their service is fake and not come out saying things like "Yeah. No" or 
+  "Sorry to hear you were misinformed" (<a href="https://twitter.com/WatchBrime/status/1283351091686457345">Tweet Here</a>) to people who
+  do not believe the service is real. On top of all of that a real company would never block anyone for believing a product is fake whenever
+  they have given 0 proofs to it even existing. They have banned multiple multiple people including myslef from discord, as well as blocking
+  people on Twitter.
+
+  </p>
+
+  <h2>Childish Twitter</h2>
+
+  <p>
+
+  A company as you know should always have a professional at the hands of the socials in order to communicate as best they can with their customers.
+  For Brime that is most certainly not the case, looking at their Twitter they make so many spelling and grammatical mistakes. As well as being just
+  plain rude to people tweeting at them if they are not in full support of Brime. If you have anything you dislike about Brime you can almost bet your
+  ass you are going to be in the wrong not Brime. While they are at it they will probably block you to! Take this quote from the same thread as before
+  "We have no interest in entertaining Epos since he had no interest in speaking to us originally and sharing misinformation. We would love to discuss 
+  anything, but has made it clear that he doesn't or didn't want too. Instead of using us to farther gain traction. The door is open." 
+  (<a href="https://twitter.com/WatchBrime/status/1283357048357314560">Link to Tweet</a>). If you also follow the Brime account and turn on their post
+  notifications you will notice they have to make the same tweet about 3 times before they can get the spelling on it correctly. Very unprofessional.
+  To top that off too they also delete old tweets if they have almost any amount of strong hate on them from the community. For example they tried
+  to do an "art contest" recently in order to get artists to make Brime new logos and banners. The work, by the way, would have been unpaid.
+
+  </p>
+
+  <p>If you are curious by the way, I could write more just lazy maybe check back later? Thanks for reading :)</p>
+</main>
